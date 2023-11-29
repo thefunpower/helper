bc227c28b00ae76cab496c1ef260d7d77341c233
sunkang <13127955213@qq.com>
2023-11-29 19:41:17 +0800

    Update Pdf.php

--- helper_v3/Pdf.php
+++ helper_v3/Pdf.php
@@ -199,7 +199,7 @@ class Pdf{
         $md5 = md5($file);
         for($i=1;$i<=$pages;$i++){
             $name = '/'.$md5.'-'.$i.'.jpg';  
-            $cmd = "gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -r300 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -sOutputFile=".$saveToDir.$name." -dFirstPage=".$i." -dLastPage=".$i." ".$file." &";
+            $cmd = "gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -r300 -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -sOutputFile=".$saveToDir.$name." -dFirstPage=".$i." -dLastPage=".$i." ".$file." ";
             exec($cmd);
             $files[] = $name;
         }   
