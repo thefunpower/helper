28d60d0820402de6315fdc9ecf777bf87399f1ae
sunkang <13127955213@qq.com>
2023-12-04 15:23:06 +0800

    Update js.php

--- inc/js.php
+++ inc/js.php
@@ -5,14 +5,18 @@
 copy_base64_data(data); 
 */
 function  copy_base64_data(){
-    return "
-    function copy_base64_data(data) {
+	global $vue;
+	$str = " 
 	    location.origin.includes(`https://`) || Message.error(`图片复制功能不可用`);
 	    data = data.split(';base64,'); let type = data[0].split('data:')[1]; data = data[1]; 
 	    let bytes = atob(data), ab = new ArrayBuffer(bytes.length), ua = new Uint8Array(ab);
 	    [...Array(bytes.length)].forEach((v, i) => ua[i] = bytes.charCodeAt(i));
 	    let blob = new Blob([ab], { type }); 
-	    navigator.clipboard.write([new ClipboardItem({ [type]: blob })]);
-	}  
+	    navigator.clipboard.write([new ClipboardItem({ [type]: blob })]); 
 	";
+	if($vue){
+		$vue->methd("copy_base64_data(data)",$str);
+		return;
+	}
+    return $str;
   }
